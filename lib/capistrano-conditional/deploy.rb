aed65a7e0113e85262780e7863b828e37e695eec
Kali Donovan <kali@deviantech.com>
2011-11-14 16:56:54 +0100

    Oops, abort is a private method

--- lib/capistrano-conditional/deploy.rb
+++ lib/capistrano-conditional/deploy.rb
@@ -16,7 +16,7 @@ class ConditionalDeploy
     if ARGV.any?{|v| v['deploy:migrations']} # If running deploy:migrations
       # If there weren't any changes to migrations or the schema file, then abort the deploy
       ConditionalDeploy.register :unneeded_migrations, :none_match => ['db/schema.rb', 'db/migrate'] do
-        context.abort "You're running migrations, but it doesn't look like you need to!"
+        context.send :abort, "You're running migrations, but it doesn't look like you need to!"
       end
     else # If NOT running deploy:migrations
       # If there were changes to migration files, run migrations as part of the deployment
