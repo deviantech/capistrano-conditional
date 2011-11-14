46ee611137837c7174e40c55695313f40773cce6
Kali Donovan <kali@deviantech.com>
2011-11-14 21:08:02 +0100

    Added check to ensure local and remote git HEADs are equal

--- lib/capistrano-conditional/integration.rb
+++ lib/capistrano-conditional/integration.rb
@@ -5,12 +5,25 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :conditional do
     desc "Initializes the conditional deployment functionality"
     task :apply do
-      log = capture("cd #{current_path} && git log --format=oneline -n 1", :pty => false)
-      ConditionalDeploy.apply_conditions!( log.split[0] )
+      deployed_hash = capture("cd #{current_path} && git log --format=oneline -n 1", :pty => false).split[0]
+      ConditionalDeploy.apply_conditions!( deployed_hash )
+    end
+    
+    desc "Tests to be sure that the newest local and remote git commits match"
+    task :ensure_latest_git do
+      remote = capture("cd #{shared_path}/cached-copy && git log --format=oneline -n 1", :pty => false)
+      local = run_locally("git log --format=oneline -n 1")
+      
+      unless local == remote
+        abort("\nLocal and remote git repositories have different HEADs:\n    Local: #{local}    Remote: #{remote}\n    Make sure you've committed your latest changes, or else pull down the remote updates and try again\n")
+      end
     end
   end
 
   # Ensure deploys apply conditional elements before running the rest of the tasks
   before 'deploy', 'conditional:apply'
   before 'deploy:migrations', 'conditional:apply'
+  
+  # Abort deployment if mismatch between local and remote git repositories
+  after 'deploy:update_code', 'conditional:ensure_latest_git'
 end
